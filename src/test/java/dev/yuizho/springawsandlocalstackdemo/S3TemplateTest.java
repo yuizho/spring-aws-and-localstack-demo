4dce33f63b72ca8b4773214d52b5525976b7f13d
yuizho <yuizho3@gmail.com>
2023-12-10 21:26:49 +0900

    configure jackson csv

--- src/test/java/dev/yuizho/springawsandlocalstackdemo/S3TemplateTest.java
+++ src/test/java/dev/yuizho/springawsandlocalstackdemo/S3TemplateTest.java
@@ -1,11 +1,16 @@
 package dev.yuizho.springawsandlocalstackdemo;
 
-
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import dev.yuizho.springawsandlocalstackdemo.csv.Jackson2CsvS3ObjectConverter;
+import io.awspring.cloud.s3.S3ObjectConverter;
 import io.awspring.cloud.s3.S3Template;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -16,22 +21,18 @@ class S3TemplateTest {
 
     @BeforeEach
     void cleanUp() {
-        s3Template.deleteObject("test-bucket", "test-bucket/person.json");
+        s3Template.deleteObject("test-bucket", "test-bucket/person.csv");
     }
 
     @Test
     void uploadFileToS3_And_downloadTheFile() {
         // given
         // upload a json file
-        s3Template.store(
-                "test-bucket",
-                "test-bucket/person.json",
-                new Person("Ito", "Yui")
-        );
+        s3Template.store("test-bucket", "test-bucket/person.csv", new Person("Ito", "Yui"));
 
         // when
         // download the json file
-        var downloaded = s3Template.read("test-bucket", "test-bucket/person.json", Person.class);
+        var downloaded = s3Template.read("test-bucket", "test-bucket/person.csv", Person.class);
 
         // then
         assertThat(downloaded.lastName).isEqualTo("Ito");
@@ -42,9 +43,20 @@ class S3TemplateTest {
         public String lastName;
         public String firstName;
 
+        public Person() {
+        }
+
         public Person(String lastName, String firstName) {
             this.lastName = lastName;
             this.firstName = firstName;
+        }
+    }
+
+    @TestConfiguration
+    static class CsvConfig {
+        @Bean
+        public S3ObjectConverter s3ObjectConverter() {
+            return new Jackson2CsvS3ObjectConverter(new CsvMapper());
         }
     }
 }
